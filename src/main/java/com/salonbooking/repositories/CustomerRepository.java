93cea85f629e48fa9bc4c2f06b7a6ad3d65675a8
Ernst95 <ernst.chapman@gmail.com>
2017-09-07 11:23:50 +0200

    - Find all ages functions added

--- src/main/java/com/salonbooking/repositories/CustomerRepository.java
+++ src/main/java/com/salonbooking/repositories/CustomerRepository.java
@@ -3,10 +3,12 @@ package com.salonbooking.repositories;
 import com.salonbooking.domain.Customer;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 /**
  * Created by Ernst on 2017/07/14.
  */
 public interface CustomerRepository extends CrudRepository<Customer, String>{
 
-
+    public List<Customer> findByAge(int age);
 }
