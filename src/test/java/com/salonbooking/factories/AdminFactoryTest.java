7d5a81d36d3c83babb19044770306b7732ca06e7
Ernst95 <ernst.chapman@gmail.com>
2017-09-07 11:29:23 +0200

    - Factory tests added

---
+++ src/test/java/com/salonbooking/factories/AdminFactoryTest.java
@@ -0,0 +1,16 @@
+package com.salonbooking.factories;
+
+import org.testng.annotations.BeforeMethod;
+
+import static org.testng.Assert.*;
+
+/**
+ * Created by Ernst on 2017/09/07.
+ */
+public class AdminFactoryTest {
+    @BeforeMethod
+    public void setUp() throws Exception {
+
+    }
+
+}
