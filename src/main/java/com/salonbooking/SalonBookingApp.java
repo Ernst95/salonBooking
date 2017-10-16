beeaefe3fab9353b33ca1b0c19274e3c0c155ace
Ernst95 <ernst.chapman@gmail.com>
2017-10-16 23:26:57 +0200

    - Security working

--- src/main/java/com/salonbooking/SalonBookingApp.java
+++ src/main/java/com/salonbooking/SalonBookingApp.java
@@ -6,10 +6,7 @@ import com.salonbooking.factories.*;
 import com.salonbooking.services.Impl.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
@@ -19,8 +16,8 @@ import java.util.List;
 /**
  * Created by Ernst on 2017/09/03.
  */
-
-@Controller
+@CrossOrigin(origins = "http://localhost:8080/")
+@RestController
 @RequestMapping(path = "/salonBooking")
 public class SalonBookingApp {
 
--- src/main/java/com/salonbooking/security/SecurityConfig.java
+++ src/main/java/com/salonbooking/security/SecurityConfig.java
@@ -1,17 +1,14 @@
 package com.salonbooking.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
 
 /**
  * Created by Ernst on 2017/09/05.
  */
 
-@Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
@@ -19,26 +16,4 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         amb.inMemoryAuthentication().withUser("user").password("user")
                 .roles("USER").and().withUser("admin").password("admin").roles("ADMIN");
     }
-
-    protected void configure(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.httpBasic().and().authorizeRequests().antMatchers("/salonBooking/deleteCustomer/**")
-                .hasRole("ADMIN").and().csrf().disable().headers().frameOptions().disable();
-        /*httpSecurity.httpBasic().and().authorizeRequests().antMatchers("/salonBooking/deleteCustomer")
-                .hasRole("ADMIN").and().csrf().disable().headers().frameOptions().disable();*/
-    }
-
-    /*@Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                .antMatchers("/salonBooking/deleteCustomer").hasRole("ADMIN")
-                .anyRequest().authenticated();
-    }
-
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-                .inMemoryAuthentication()
-                .withUser("user").password("password").roles("ADMIN");
-    }*/
 }
